14ffb99303f9320c6fcf79762750892eb7cd9f59
amanahmed97 <aman.ahmed97@gmail.com>
2020-01-27 14:12:46 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
+import {MatInputModule,MatFormFieldModule, MatToolbarModule, MatCheckboxModule, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatAutocompleteModule} from '@angular/material'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout'
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    //AppRoutingModule, 
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCheckboxModule,
+    FlexLayoutModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatAutocompleteModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
